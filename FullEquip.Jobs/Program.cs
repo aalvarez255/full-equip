4c08caa27ee182c40c28ba0820232f320521d265
Adrià Álvarez Guerra <adria.alvarez@itequia.com>
2019-06-14 15:22:28 +0200

    setup

---
+++ FullEquip.Jobs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FullEquip.Jobs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
