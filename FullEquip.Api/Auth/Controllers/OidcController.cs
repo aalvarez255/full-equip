48bfcff90d07eb6f38329dc87b8a7884d389c126
Adrià Álvarez Guerra <adria.alvarez@itequia.com>
2019-07-22 12:15:09 +0200

    azure  login

--- FullEquip.Api/Auth/Controllers/OidcController.cs
+++ FullEquip.Api/Auth/Controllers/OidcController.cs
@@ -1,5 +1,11 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Configuration;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
 
 namespace FullEquip.Api.Auth.Controllers
 {
@@ -18,7 +24,7 @@ namespace FullEquip.Api.Auth.Controllers
         public IActionResult Oidc()
         {
             return Redirect(
-                $"https://login.microsoftonline.com/{_config["AzureAD:TenantId"]}/oauth2/authorize" 
+                $"https://login.microsoftonline.com/{_config["AzureAD:TenantId"]}/oauth2/authorize"
                     + $"?client_id={_config["AzureAD:ClientId"]}"
                     + $"&redirect_uri={_config["AzureAD:RedirectUri"]}"
                     + "&scope=openid%20profile%20email"
@@ -26,10 +32,39 @@ namespace FullEquip.Api.Auth.Controllers
             );
         }
 
-        [HttpGet("login")]
-        public IActionResult Login(string code, string session_state)
+        [HttpGet("code")]
+        public async Task<IActionResult> Code(string code, string session_state, string token)
+        {
+            if (String.IsNullOrEmpty(code) || String.IsNullOrEmpty(session_state))
+                return Unauthorized();
+
+            var s = await GetAzureAccessToken(code);
+            return Ok();
+        }
+
+        [HttpGet("token")]
+        public async Task<IActionResult> Token(string code, string token)
         {
             return Ok();
+        }
+
+        private async Task<string> GetAzureAccessToken(string code)
+        {
+            using (var client = new HttpClient())
+            {
+                client.BaseAddress = new Uri($"https://login.microsoftonline.com");
+                var content = new FormUrlEncodedContent(new[]
+                {
+                    new KeyValuePair<string, string>("grant_type","authorization_code"),
+                    new KeyValuePair<string, string>("code", code),
+                    new KeyValuePair<string, string>("client_id", _config["AzureAD:ClientId"]),
+                    new KeyValuePair<string, string>("client_secret", _config["AzureAD:ClientSecret"]),
+                    new KeyValuePair<string, string>("redirect_uri", _config["AzureAD:RedirectUri"])
+                });
+                var result = await client.PostAsync($"/{ _config["AzureAD:TenantId"]}/oauth2/token", content);
+                var json = JObject.LoadAsync(await result.Content.ReadAsStringAsync());
+                return json
+            }
         }
     }
 }
